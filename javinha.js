63738bd351e63967f824601b3e355baaac970e99
Yasmin <yasmin.cristina.ifsp@gmail.com>
2022-02-03 09:38:35 -0300

    feat: projeto

---
+++ javinha.js
@@ -0,0 +1,22 @@
+function cor(){
+    document.getElementById("cx").style.
+    backgroundColor = "#B0E0E6";
+    document.getElementById("fundo").style.
+    backgroundColor = "#00BFFF";
+  document.getElementById("corpo").style.
+    backgroundColor = "#B0E0E6";}
+  function cor2(){
+    document.getElementById("cx").style.
+    backgroundColor = "#ffcbdb";
+    document.getElementById("fundo").style.
+    backgroundColor = "#FF1493";
+  document.getElementById("corpo").style.
+    backgroundColor = "#ffcbdb";}
+   
+  function plano(){window.open('lampada.html');}
+   
+  function acender(){
+  document.getElementById("imagem").src = "https://image.freepik.com/vetores-gratis/lampada-iluminada-realista-isolada_1284-41774.jpg";
+  }
+  
+  
